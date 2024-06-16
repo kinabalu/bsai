df10e86731058e3843094212ae94a9045cc35d82
Joseph Ottinger <joeo@enigmastation.com>
2024-06-16 10:43:46 -0400

    Chapter 2 is getting some content wooo

--- book/src/main/java/com/apress/bsai/BookApplication.java
+++ book/src/main/java/com/apress/bsai/BookApplication.java
@@ -1,27 +1,17 @@
 package com.apress.bsai;
 
-import com.apress.bsai.service.Publisher;
-import org.apache.commons.io.IOUtils;
-import org.apache.logging.log4j.message.StringFormattedMessage;
+import com.apress.bsai.service.OperationsService;
 import org.asciidoctor.Asciidoctor;
-import org.asciidoctor.Options;
-import org.asciidoctor.ast.Document;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
-import org.zeroturnaround.exec.ProcessExecutor;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.TimeoutException;
 
 @SpringBootApplication
 public class BookApplication {
@@ -39,45 +29,9 @@ public class BookApplication {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(List<String> resourceNames, Asciidoctor asciidoctor, ApplicationContext ctx) {
-        return args -> {
-            var publishers=resourceNames.stream().sorted().map((name) ->
-                    new Publisher(asciidoctor, name, "./target", "generated-content")).toList();
-            publishers.forEach(Publisher::processToDocbook);
-            publishers.forEach(Publisher::processToDocX);
-
-            System.exit(0);
-        };
+    public OperationsService getOperationsService(Asciidoctor asciidoctor, List<String> resources) {
+        return new OperationsService(asciidoctor, resources);
     }
-
-//    private String buildOutput(Asciidoctor asciidoctor, String name) {
-//        System.out.println(name);
-//        try (InputStream in = this.getClass().getResourceAsStream("/" + name);
-//             InputStreamReader reader = new InputStreamReader(in, StandardCharsets.UTF_8)) {
-//            var directory = new File("./target/asciidoctor-output");
-//            directory.mkdirs();
-//            // TODO correct name to include .xml
-//            var outputFile = new File(directory, removeFileExtension(name, false) + ".xml");
-//            var writer = new FileWriter(outputFile);
-//            var options = Options.builder()
-//                    .compact(true)
-//                    .backend("docbook")
-//                    .build();
-//            asciidoctor.convert(reader, writer, options);
-//            return outputFile.getAbsolutePath();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    private String readContent(String name) {
-//        try (InputStream in = this.getClass().getResourceAsStream("/" + name)) {
-//            var doc = IOUtils.readLines(Objects.requireNonNull(in), StandardCharsets.UTF_8);
-//            return String.join("\n", doc);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e.getMessage(), e);
-//        }
-//    }
 
     public static void main(String[] args) {
         SpringApplication.run(BookApplication.class, args);
--- book/src/main/java/com/apress/bsai/service/Publisher.java
+++ book/src/main/java/com/apress/bsai/service/Publisher.java
@@ -1,7 +1,9 @@
 package com.apress.bsai.service;
 
+import org.apache.commons.io.IOUtils;
 import org.asciidoctor.Asciidoctor;
 import org.asciidoctor.Options;
+import org.asciidoctor.ast.Document;
 import org.zeroturnaround.exec.ProcessExecutor;
 
 import java.io.*;
@@ -10,12 +12,14 @@ import java.util.Arrays;
 import java.util.concurrent.TimeoutException;
 
 public class Publisher {
+    static int totalWordCount=0;
     private final Asciidoctor asciidoctor;
     private final String fileName;
     private final File workDir;
     private final File targetDir;
     private final String baseName;
     private String workingName;
+    private Document document;
 
     public Publisher(Asciidoctor asciidoctor, String fileName, String workDir, String targetDir) {
         this.asciidoctor = asciidoctor;
@@ -32,33 +36,63 @@ public class Publisher {
             return filename;
         }
 
-        String extPattern = "(?<!^)[.]" + (removeAllExtensions ? ".*" : "[^.]*$");
+        var extPattern = "(?<!^)[.]" + (removeAllExtensions ? ".*" : "[^.]*$");
         return filename.replaceAll(extPattern, "");
     }
 
-    public void processToDocbook() {
-        try (InputStream in = this.getClass().getResourceAsStream("/" + fileName);
-             InputStreamReader reader = new InputStreamReader(in, StandardCharsets.UTF_8)) {
-            workingName = baseName + ".xml";
-            var outputFile = new File(targetDir, workingName);
-            var writer = new FileWriter(outputFile);
+    public Document buildDocument() {
+        try (var in = this.getClass().getResourceAsStream("/" + fileName);
+             var reader = new InputStreamReader(in, StandardCharsets.UTF_8)) {
+            var content = String.join("\n", IOUtils.readLines(reader));
             var options = Options.builder()
                     .compact(true)
                     .backend("docbook")
                     .build();
-            asciidoctor.convert(reader, writer, options);
+            document = asciidoctor.load(content, options);
+
+            return document;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public void processToDocbook() {
+        workingName = baseName + ".xml";
+        var outputFile = new File(targetDir, workingName);
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(outputFile);
+            IOUtils.write(document.convert(), writer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        // need to get word count
+        // need to
+    }
+
+    synchronized public int getWordCount() {
+        int wordCount=0;
+        // TODO implement this!
+
+
+        // each block has a bunch of nodes. Convert to text and count words, eh.
+
+        totalWordCount+=wordCount;
+        return wordCount;
+    }
+
+    public int getFleschKincaid() {
+        // TODO implement
+        return 0;
+    }
+
     public void processToDocX() {
-        File input=new File(targetDir, workingName);
+        var input = new File(targetDir, workingName);
         new File("./target/content").mkdirs();
         var command = new String[]{"pandoc", "-r", "docbook", "-t", "docx", "-o", "./target/content/" + baseName + ".docx", "--lua-filter", "src/main/resources/lua/admonitions.lua", input.getPath()};
         System.out.println(Arrays.toString(command));
         try {
-            String output=new ProcessExecutor().command(command).readOutput(true).execute().outputUTF8();
+            var output = new ProcessExecutor().command(command).readOutput(true).execute().outputUTF8();
             System.out.println(output);
         } catch (IOException e) {
             throw new RuntimeException(e);
