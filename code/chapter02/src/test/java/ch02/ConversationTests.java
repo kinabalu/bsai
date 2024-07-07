60bd54eea3366d52c799dd3abb3744e94c61cec1
Joseph Ottinger <joeo@enigmastation.com>
2024-07-07 13:12:01 -0400

    better model for conversation

--- code/chapter02/src/test/java/ch02/ConversationTests.java
+++ code/chapter02/src/test/java/ch02/ConversationTests.java
@@ -1,47 +1,62 @@
 package ch02;
 
 import ch02.service.ConversationChatService;
-import com.google.common.collect.Iterables;
-import org.assertj.core.util.Lists;
-import org.junit.jupiter.api.Test;
+import org.apache.commons.text.WordUtils;
+import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.ai.chat.messages.AssistantMessage;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.Generation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ConversationTests {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
     @Autowired
     ConversationChatService conversationChatService;
 
-    List<Message> buildQuery() {
-        return List.of(
-                new UserMessage("How can I fit a square peg into a round hole?")
-        );
+    private List<Generation> display(List<Generation> results) {
+        var content = results.getFirst().getOutput().getContent();
+        Arrays.stream(
+                        WordUtils
+                                .wrap(content, 62, "\n", true)
+                                .split("\\n")
+                )
+                .forEach(log::info);
+        log.info("-----");
+        return results;
     }
 
     @Test
+    @Order(1)
     void runQuery() {
-        var results = conversationChatService.converse(buildQuery());
-        System.out.println(results);
+        display(conversationChatService.converse(List.of(
+                new UserMessage("How can I fit a square peg into a round hole?")
+        )));
     }
 
     @Test
+    @Order(2)
     void runQuery2() {
-        var firstIteration = conversationChatService.converse(buildQuery());
-        List<Message> messages = Lists.newArrayList(Iterables.concat(
-                List.of(new UserMessage("How can I fit a square peg into a round hole?")),
-                firstIteration.stream().map((generation) -> new AssistantMessage(generation.getOutput().getContent())).toList(),
-                List.of(new UserMessage("Does it make a difference if it's in the shape of a bird?"))
+        // we want to make a mutable list, because we're adding context.
+        List<Message> messages = new ArrayList<>(List.of(
+                new UserMessage("How can I fit a square peg into a round hole?")
         ));
-        var results = conversationChatService.converse(messages);
-        System.out.println(results.getFirst().getOutput().getContent());
-        //return results;
+        var results = display(conversationChatService.converse(messages));
+
+        // we want to establish the context of the first answer.
+        messages.add(results.getFirst().getOutput());
+        // now we want to add some extra context of our own...
+        messages.add(
+                new UserMessage("Does it make a difference if it's in the shape of a bird?")
+        );
+        display(conversationChatService.converse(messages));
     }
 }
