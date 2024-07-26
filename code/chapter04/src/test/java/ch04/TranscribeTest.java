02aef2bc6f5b35e0b94d0a92aab958bf575b9c1b
Andrew Lombardi <andrew@mysticcoders.com>
2024-07-26 08:24:57 -0700

    chapter05 changes

--- code/chapter04/src/test/java/ch04/TranscribeTest.java
+++ code/chapter04/src/test/java/ch04/TranscribeTest.java
@@ -12,7 +12,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-
 public class TranscribeTest {
 
     @Autowired
