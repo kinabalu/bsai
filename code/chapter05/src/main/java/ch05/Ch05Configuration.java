8ba53a5b0e47e7c61bef7fbea564d0a9c8f176b0
Andrew Lombardi <andrew@mysticcoders.com>
2024-08-03 14:10:50 -0700

    make suggested changes to chapter4 and some code for chapter 5

---
+++ code/chapter05/src/main/java/ch05/Ch05Configuration.java
@@ -0,0 +1,23 @@
+package ch05;
+
+import org.springframework.ai.openai.OpenAiImageModel;
+import org.springframework.ai.openai.api.OpenAiImageApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+public class Ch05Configuration {
+
+    @Bean
+    OpenAiImageModel imageModel(@Value("${spring.ai.openai.api-key}") String apiKey) {
+        return new OpenAiImageModel(new OpenAiImageApi(apiKey));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch05Configuration.class, args);
+    }
+}
